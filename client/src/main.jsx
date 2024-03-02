0891a24d60b2803e847fd49f7b0788ef9410ac18
[Fatih Hakan Direk] <[fatihhakandirek@gmail.com]>
2024-03-02 20:02:36 +0300

    admin layout created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout.jsx";
-import CartProvider from "./context/CartProvider.jsx";
-import App from "./App.jsx";
+import { Layout } from "./layouts/Layout";
+import CartProvider from "./context/CartProvider";
+import App from "./App";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
@@ -10,9 +10,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <CartProvider>
-      <MainLayout>
+      <Layout>
         <App />
-      </MainLayout>
+      </Layout>
     </CartProvider>
   </BrowserRouter>
 );
